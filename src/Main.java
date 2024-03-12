0d797b45ecf907c5ef59a56dcd8d3c567183b3aa
Tharusha Thilakshan <tharushajayasekara04@gmail.com>
2024-03-12 15:43:41 +0530

    Encapsulation

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Person p1=new Person();
+        p1.setName("Tharusha Thilakshan Jayasekara");
+        p1.setAge(23);
+
+        System.out.println("My name is " + p1.getName() + " and My age is " + p1.getAge());
+
+
+        }
+    }
